7922ba0da70a75d4bf1ca1699d723a5d6ee54461
Administrador <Administrador@DESKTOP-ARL2874>
2026-04-29 10:09:03 -0300

    Adicionar arquivos de projeto.

---
+++ AdoteMeApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AdoteMeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AdoteMeApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AdoteMeApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
